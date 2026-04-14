22ec99d7e0085d32c32d415d8711bbb16f20fb0c
Gregory Arapis <ga@gregar.dev>
2026-04-14 22:00:49 +0300

    working on hero play

--- src/components/hero/heroadvantages/HeroAdvantages.tsx
+++ src/components/hero/heroadvantages/HeroAdvantages.tsx
@@ -1,47 +1,31 @@
 import { Box, Card, Divider, Table, Typography } from "@mui/joy";
-import advantages from '../../../data/advantages.json';
+import advantages from "../../../data/advantages.json";
 
-export default function HeroAdvantages(){
-
-    return(
+export default function HeroAdvantages() {
+    return (
         <Box>
             <Typography>Advantages</Typography>
             <Divider />
-            <Card
-                sx={{m:1}}
-                variant="soft"
-                size="lg"
-            >
+            <Card sx={{ m: 1 }} variant="soft" size="lg">
                 <Table color="primary">
                     <thead>
-                        <th>
-                            Advantage
-                        </th>
-                        <th>
-                            Type
-                        </th>
-                        <th>
-                            Effect
-                        </th>
+                        <th>Advantage</th>
+                        <th>Type</th>
+                        <th>Effect</th>
+                        <th>Source</th>
                     </thead>
                     <tbody>
-                        {advantages.map((adv)=>(
+                        {advantages.map((adv) => (
                             <tr>
-                                <td>
-                                    {adv.name}
-                                </td>
-                                <td>
-                                    {adv.type}
-                                </td>
-                                <td>
-                                    {adv.effect}
-                                </td>
+                                <td>{adv.name}</td>
+                                <td>{adv.type}</td>
+                                <td>{adv.effect}</td>
+                                <td>{adv.source}</td>
                             </tr>
                         ))}
                     </tbody>
                 </Table>
-
             </Card>
         </Box>
-    )
+    );
 }
--- src/components/hero/heroplay/HeroPlay.tsx
+++ src/components/hero/heroplay/HeroPlay.tsx
@@ -1,11 +1,52 @@
-import { Box, Divider, Typography } from "@mui/joy";
+import { Box, Card, Divider, Link, Sheet, Stack, Typography } from "@mui/joy";
+import useActiveHero from "../../../hooks/useActiveHero";
+import type { Hero } from "../../../interfaces/hero";
 
-export default function HeroPlay(){
+const abilities = [
+    { label: "Strength", id: "str" },
+    { label: "Stamina", id: "sta" },
+    { label: "Agility", id: "agi" },
+    { label: "Dexterity", id: "dex" },
+    { label: "Fighting", id: "fgt" },
+    { label: "Intellect", id: "int" },
+    { label: "Awereness", id: "awe" },
+    { label: "Presense", id: "pre" },
+];
 
-    return(
+function roll() {}
+
+export default function HeroPlay() {
+    const { activeHero } = useActiveHero();
+    return (
         <Box>
             <Typography>In Play</Typography>
             <Divider />
+            <Card size="lg" variant="soft">
+                <Stack direction="row">
+                    {abilities.map((ab) => (
+                        <Card variant="solid" color="primary" sx={{ m: 1 }}>
+                            <Link overlay onClick={roll}>
+                                <Stack
+                                    direction="column"
+                                    sx={{
+                                        width: 100,
+                                        justifyContext: "center",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <Typography>{ab.label}</Typography>
+                                    <Typography level="h1">
+                                        {
+                                            (activeHero[ab.id as keyof Hero] ??
+                                                0) as string
+                                        }
+                                    </Typography>
+                                </Stack>
+                            </Link>
+                        </Card>
+                    ))}
+                </Stack>
+            </Card>
         </Box>
-    )
+    );
 }
