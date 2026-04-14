cde267cd524957b2ccd81c9dd06d02a08d151191
Gregory Arapis <ga@gregar.dev>
2026-04-14 13:34:01 +0300

    core advantages data added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer(){
 
     return(
         <Typography>
-            <a href="https://www.flaticon.com/free-icons/strong" title="strong icons">Strong icons created by Leremy - Flaticon</a> - OGL Game Rules via <a href="https://www.d20herosrd.com/">d20herosrd</a> and Mutants and Masterminds OGL by Green Ronin Studios
+            <a href="https://www.flaticon.com/free-icons/strong" title="strong icons">Strong icons created by Leremy - Flaticon</a> - OGL Game Rules via Mutants and Masterminds OGL by Green Ronin Studios
         </Typography>
     )
 }
